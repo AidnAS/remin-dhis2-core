014b358222bad2b06ceb900af3dc4f307eec0671
Vegard <vegard.deila@gmail.com>
2024-07-03 15:49:26 +0200

    Try without the init script

--- dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/container/DhisPostgreSQLContainer.java
+++ dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/container/DhisPostgreSQLContainer.java
@@ -57,7 +57,6 @@ public class DhisPostgreSQLContainer<SELF extends DhisPostgreSQLContainer<SELF>>
         addEnv( "POSTGRES_USER", getUsername() );
         addEnv( "POSTGRES_PASSWORD", getPassword() );
         setCommand( getPostgresCommandWithCustomConfigs() );
-        withInitScript( "db/extensions.sql" );
     }
 
     private String getPostgresCommandWithCustomConfigs()
